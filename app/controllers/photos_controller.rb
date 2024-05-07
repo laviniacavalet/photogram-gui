a2ef4ed9a92209e54b372ad44f8aecda23b57bbc
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-05-07 03:33:32 +0000

    photos show works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,6 +10,18 @@ class PhotosController < ApplicationController
 
   end
 
+  def show
+    url_id = params.fetch("path_id")
+  
+    matching_photos = Photo.where({ :id => url_id })
+
+    @the_photo = matching_photos.at(0)
+
+
+    render({ :template => "photo_templates/show" })
+
+  end
+
 
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ get("/users", { :controller => "users", :action => "index" })
 get("/users/:path_username", { :controller => "users", :action => "show" })
 
 get("/photos", { :controller => "photos", :action => "index" })
+get("/photos/:path_id", { :controller => "photos", :action => "show" })
 
 
 end

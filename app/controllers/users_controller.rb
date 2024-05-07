8c91e7ab75d0d1d6fe6d31445a1d5642d9647ecc
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-05-07 03:07:09 +0000

    user details looks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
   @list_of_users = matching_users.order({ :username => :asc })
     render({ :template => "user_templates/index" })
   end
+
+  def show
+   url_username = params.fetch("path_username")
+
+   matching_username = User.where({ :username => url_username })
+
+   @the_user = matching_username.at(0)
+    render({ :template => "user_templates/show" }) 
+  end
 end

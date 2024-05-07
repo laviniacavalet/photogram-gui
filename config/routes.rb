cf6da85fdbb5dff08a76cf0aa2df85620002878c
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-05-07 04:11:12 +0000

    photo create works

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ get("/users/:path_username", { :controller => "users", :action => "show" })
 
 get("/photos", { :controller => "photos", :action => "index" })
 get("/photos/:path_id", { :controller => "photos", :action => "show" })
+get("/delete_photo/:toast_id",{ :controller => "photos", :action => "bye" })
+get("/insert_photo",{ :controller => "photos", :action => "create" })
 
 
 end
